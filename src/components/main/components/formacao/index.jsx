950165295be64aa5b338beb7e11fe5ea929f1275
TalisDuarte007 <tcd07@hotmail.com>
2023-09-20 20:18:12 -0300

    atualizei os links, para abrirem em uma nova guia

--- src/components/main/components/formacao/index.jsx
+++ src/components/main/components/formacao/index.jsx
@@ -12,24 +12,24 @@ export default function Formacao(props){
 
                 <StyledDivFormacoes>
                     <h3>Unopar</h3>
-                    <a className="linkAcessoDiploma"href="https://drive.google.com/file/d/1Q0dQNkwWR8mCa3ICEcvMQrxlfZPxJSCU/view?usp=drive_link">ADS (Análise e Desenvolvimento de Sistemas)</a>
+                    <a className="linkAcessoDiploma" target="_blank" href="https://drive.google.com/file/d/1Q0dQNkwWR8mCa3ICEcvMQrxlfZPxJSCU/view?usp=drive_link" rel="noreferrer">ADS (Análise e Desenvolvimento de Sistemas)</a>
                     <p className="textoFormacoes">Tecnólogo em Análise e Desenvolvimento de Sistemas. Concluído em Junho de 2023.</p>
                 </StyledDivFormacoes>
                 <StyledHR/>
                 <StyledDivFormacoes>
                         <h3>Alura</h3>
-                        <a className="linkAcessoDiploma" href="https://cursos.alura.com.br/degree/certificate/f994e2da-a4b3-43a0-bdcf-79eac2824cde">Formação Front-End</a>
+                        <a className="linkAcessoDiploma" target="_blank" href="https://cursos.alura.com.br/degree/certificate/f994e2da-a4b3-43a0-bdcf-79eac2824cde" rel="noreferrer">Formação Front-End</a>
                         <p className="textoFormacoes">Curso de 115horas focado em Front-End com HTML, CSS e JavaScript.</p>
                 </StyledDivFormacoes>
                 <StyledHR/>
                 <StyledDivFormacoes>
                     <h3>Digital Innovation One</h3>
                     <div>
-                        <a className="linkAcessoDiploma" href="https://www.dio.me/certificate/5E04ACF2/share">Santander Bootcamp Fullstack Developer</a>
+                        <a className="linkAcessoDiploma" target="_blank" href="https://www.dio.me/certificate/5E04ACF2/share" rel="noreferrer">Santander Bootcamp Fullstack Developer</a>
                         <p className="textoFormacoes">Bootcamp de 120horas focado em FullStack com Angular e Java.</p>
                     </div>
                     <div>
-                        <a className="linkAcessoDiploma" href="https://www.dio.me/certificate/7D833A67/share">Geração Tech Unimed-BH</a>
+                        <a className="linkAcessoDiploma" target="_blank" href="https://www.dio.me/certificate/7D833A67/share" rel="noreferrer">Geração Tech Unimed-BH</a>
                         <p className="textoFormacoes">Bootcamp de 127horas focado em FullStack com Angular, MySQL e .NET.</p>
                     </div>
                 </StyledDivFormacoes>
